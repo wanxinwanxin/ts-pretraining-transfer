148882341f32962dcd7ab4871e1eb3b4a03d1ccc
wanxinwanxin <nflswanxin@gmail.com>
2026-08-05 12:31:18 -0400

    1.8 toy models

--- scripts/train_toy_model.py
+++ scripts/train_toy_model.py
@@ -1,12 +1,36 @@
 from torch.utils.data import DataLoader
+from ts_transfer.data.toy import ToyDataset
+from ts_transfer.models.toy import TwoLayerMLP
+import torch.optim as optim
+import torch.nn as nn
+import torch
 
-from src.ts_transfer.models.toy import custom_toy_dataset
 
 # generate data
-train_dataloader = DataLoader(custom_toy_dataset(seed=42, n=1000, noise_std=0.1), batch_size=10, shuffle=True)
-test_dataloader = DataLoader(custom_toy_dataset(seed=42, n=1000, noise_std=0.1), batch_size=10, shuffle=True)
+train_dataloader = DataLoader(ToyDataset(seed=42, n=1000, noise_std=0.1), batch_size=10, shuffle=True)
+test_dataloader = DataLoader(ToyDataset(seed=142, n=1000, noise_std=0.1,), batch_size=10, shuffle=False)
 
+torch.manual_seed(42)
+model = TwoLayerMLP(input_size=1, hidden_size=32, output_size=1)
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+loss_fn = nn.MSELoss()
 
 
 # train model
+for epoch in range(200):
+    total_loss = 0
+    for batch in train_dataloader:
+        x, y = batch
+        y_pred = model(x)
+        optimizer.zero_grad()
+        loss = loss_fn(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+
+    if epoch % 10 == 0:
+            print(f"Epoch {epoch}, Train Loss: {total_loss / len(train_dataloader)}")
+    
+
+
 
--- src/ts_transfer/data/toy.py
+++ src/ts_transfer/data/toy.py
@@ -2,23 +2,23 @@
 # in src/ts_transfer/data/toy.py, seeded and reproducible.
 
 import torch
-import torch.nn as nn
 from torch.utils.data import Dataset
 
 
-
-class custom_toy_dataset(Dataset):
+class ToyDataset(Dataset):
     def __init__(self, seed=42, n=1000, noise_std=0.1):
-        self.generator = torch.Generator()
-        self.seed = seed
+        self.generator = torch.Generator().manual_seed(seed)
         self.n = n
         self.noise_std = noise_std
+        x = torch.rand(n, generator=self.generator) * 2 - 1
+        self.x = x.unsqueeze(1)
+        self.y = 2 * x + torch.randn(n, generator=self.generator) * self.noise_std
+        self.y = self.y.unsqueeze(1)
 
     def __len__(self):
         return self.n
 
     def __getitem__(self, idx):
-        self.generator.manual_seed(idx+self.seed)
-        x = torch.rand(1, generator=self.generator, requires_grad=False) * 2 - 1
-        y = 2 * x + torch.randn(1, generator=self.generator) * self.noise_std
+        x = self.x[idx]
+        y = self.y[idx]
         return x, y
